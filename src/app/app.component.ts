f43ff142fec2e7b218161b732b851069c75ee326
Mohamed Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-06-17 16:15:17 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'membrain';
+  class = 'notscrolled';
+  first = false;
+  onMouseWheel(event:any){
+    if (event.deltaY > 0 && !this.first){
+      this.class="scrolled";
+      this.first=true;
+    }else if(event.deltaY > 0){
+      //goes right
+
+    
+    }else if (event.deltaY < 0){
+      //goes left
+    }
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
+import { ExploreComponent } from './explore/explore.component';
+import { ScrollerComponent } from './scroller/scroller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TitleComponent,
+    ExploreComponent,
+    ScrollerComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: Window, useValue: window }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

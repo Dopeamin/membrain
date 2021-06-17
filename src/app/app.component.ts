5275db4b41ef59e30b0566fbb93238a757245eda
Mohamed Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-06-17 18:03:52 +0100

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +8,12 @@ export class AppComponent {
   title = 'membrain';
   class = 'notscrolled';
   first = false;
+  x:number;
+  y:number;
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+  }
   onMouseWheel(event:any){
     if (event.deltaY > 0 && !this.first){
       this.class="scrolled";
@@ -21,4 +27,10 @@ export class AppComponent {
     }
     
   }
+
+    @HostListener('document:mousemove', ['$event']) 
+    onMouseMove(e:any) {
+      this.x = e.clientX-6;
+      this.y = e.clientY-5;
+    }
 }
